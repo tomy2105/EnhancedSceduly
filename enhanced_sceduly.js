796d650c6dc7fe9ee5c81ced5e073ed8414bf6c8
tomy2105 <t.petrovic@inet.hr>
2024-02-27 11:58:16 +0100

    Added ability to filter by day of week + some refactoring and Croatian translation

--- enhanced_sceduly.js
+++ enhanced_sceduly.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Enhanced Sceduly
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Enables selective hiding and displaying of schedule items in Sceduly based on title and group
 // @author       Tomislav Petrović <t.petrovic@inet.hr>
 // @match        https://sceduly.com/hr/?page=Profile*
+// @match        https://sceduly.com/?page=Profile*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=sceduly.com
 // @require      http://code.jquery.com/jquery-latest.js
 // @grant        none
@@ -14,6 +15,11 @@
 (function() {
     'use strict';
 
+    let paramNames = {"Colors": "Colors", "Address" : "Address", "Group" : "Group", "No Headers" : "No Headers" };
+    if (window.location.href.indexOf('/hr/') !== -1) {
+        paramNames = {"Colors": "Boje", "Address" : "Adresa", "Group" : "Grupa", "No Headers" : "Bez zaglavlja" };
+    }
+
     const contentContainerTopPadding = $('#contentContainer').css('padding-top');
     const contentContainerLeftPadding = $('#contentContainer').css('padding-left');
 
@@ -21,14 +27,21 @@
         return node.find(".title").text().trim() + " " + node.find(".group").text().trim();
     }
 
+    function showOrHide(element, show)
+    {
+        if (show) {
+            element.show();
+        } else {
+            element.hide();
+        }
+    }
+
     function showHideAll(subjects, classes, params)
     {
         $('.event').each(function(i, obj) {
             const subject = getSubject($(this));
-            if (!subjects[subject])
-                $(this).hide();
-            else
-                $(this).show();
+            showOrHide($(this), subjects[subject]);
+
             if (params["Colors"] === false) {
                 $(this).attr('class', 'event');
                 $(this).css("border-bottom", "1px solid #ccc");
@@ -36,6 +49,7 @@
                 $(this).css('');
                 $(this).attr('class', classes[subject]);
             }
+
             let classRoom = $(this).find('.classroom');
             let classRoomBack = $(this).find('.classroomback');
             if (params["Address"] === true) {
@@ -44,25 +58,25 @@
                 let classRoomText = classRoomBack.text();
                 classRoom.text(classRoomText.substring(0, classRoomText.indexOf('@')));
             }
-            if (params["Group"] === false) {
-                $(this).find('.group').hide();
-            } else {
-                $(this).find('.group').show();
-            }
-            if (params["No Headers"] === true) {
-                $("#headerContainer").hide();
-                $("#acpMenu").hide();
-                $(".contentSection.noPrint").hide();
-                $("#contentContainer").css('padding-top', '0px');
-                $("#contentContainer").css('padding-left', '0px');
-            } else {
-                $("#headerContainer").show();
-                $("#acpMenu").show();
-                $(".contentSection.noPrint").show();
-                $("#contentContainer").css('padding-top', contentContainerTopPadding);
-                $("#contentContainer").css('padding-left', contentContainerLeftPadding);
-            }
+        });
 
+        showOrHide($("#headerContainer"), !params["No Headers"]);
+        showOrHide($("#acpMenu"), !params["No Headers"]);
+        showOrHide($(".contentSection.noPrint"), !params["No Headers"]);
+        if (params["No Headers"] === true) {
+            $("#contentContainer").css('padding-top', '0px');
+            $("#contentContainer").css('padding-left', '0px');
+        } else {
+            $("#contentContainer").css('padding-top', contentContainerTopPadding);
+            $("#contentContainer").css('padding-left', contentContainerLeftPadding);
+        }
+
+        const ths = $('.calendar.month > table > tbody > tr:first-child > th')
+        ths.each(function(i, obj) {
+            showOrHide($(this), params["Day"+ (i % ths.length)]);
+        });
+        $('.calendar.month > table > tbody > tr > td').each(function(i, obj) {
+            showOrHide($(this), params["Day"+ (i % ths.length)]);
         });
     }
 
@@ -73,14 +87,28 @@
         return newBlock;
     }
 
+    function createCheckbox(params, storageKeyName, paramsDiv, id, param, text, update) {
+        var newBlock = createQBlock();
+        var input = $('<input />', { type: 'checkbox', id: id, checked: params[param] });
+        input.change(function() {
+            params[param] = $(this).is(':checked');
+            localStorage.setItem(storageKeyName, JSON.stringify(params));
+            if (update !== undefined) update();
+        });
+        input.appendTo(newBlock);
+        $('<label />', { 'for': id, text: " " + text }).appendTo(newBlock);
+        newBlock.appendTo(paramsDiv);
+    }
 
-    let lastLocation = "";
     var intervalId = window.setInterval(function(){
         const localStorageKeyName = "subjectsState";
         const localStorageParamKeyName = "paramsState";
         clearInterval(intervalId);
 
-        let initialParams = {"Colors": true, "Address" : true, "Group" : true, "No Headers" : false };
+        let initialParams = Object.keys(paramNames).reduce(function (map, key) { map[key] = true; return map; }, {});
+        $('.calendar.month > table > tbody > tr:first-child > th').each(function(i, obj) {
+            initialParams["Day"+i] = true;
+        });
         let params = {...initialParams};
         params = JSON.parse(localStorage.getItem(localStorageParamKeyName));
         if (params === null || params.constructor !== Object) {
@@ -92,14 +120,15 @@
         if (subjects === null || subjects.constructor !== Object) {
             subjects = {};
         }
+
         let classes = {};
         $('.event').each(function(i, obj) {
             const subject = getSubject($(this));
             if (!(subject in subjects))
                 subjects[subject] = true;
-            if (!subjects[subject])
-                $(this).hide();
+
             classes[subject] = $(this).attr('class');
+
             const classroomBackupDiv = $('<div/>');
             classroomBackupDiv.addClass('classroomback');
             classroomBackupDiv.text($(this).find('.classroom').text().trim());
@@ -111,35 +140,23 @@
         newDiv.addClass('noPrint');
         var paramsDiv = $('<div/>');
         $.each(Object.keys(params).sort(), function(i, param) {
-            var newBlock = createQBlock();
-            var input = $('<input />', { type: 'checkbox', id: param, checked: params[param] });
-            input.change(function() {
-                params[param] = $(this).is(':checked');
-                localStorage.setItem(localStorageParamKeyName, JSON.stringify(params));
-                showHideAll(subjects, classes, params);
-            });
-            input.appendTo(newBlock);
-            $('<label />', { 'for': param, text: " " + param }).appendTo(newBlock);
-            newBlock.appendTo(paramsDiv);
+            if (!param.startsWith("Day")) {
+                createCheckbox(params, localStorageParamKeyName, paramsDiv, param, param, paramNames[param], function() { showHideAll(subjects, classes, params); });
+            }
+        });
+        $('.calendar.month > table > tbody > tr:first-child > th').each(function(i, obj) {
+            createCheckbox(params, localStorageParamKeyName, paramsDiv, "Day" + i, "Day" + i, $(this).text(), function() { showHideAll(subjects, classes, params); });
         });
         paramsDiv.appendTo(newDiv);
+
         const divider = $('<div/>');
         divider.height('2px');
         divider.css('background-color', '#ccc');
         divider.appendTo(newDiv);
+
         var subjectDiv = $('<div/>');
         $.each(Object.keys(subjects).sort(), function(i, subject) {
-            const isChecked = subjects[subject];
-            var newBlock = createQBlock();
-            var input = $('<input />', { type: 'checkbox', id: 'cb'+i, checked: isChecked });
-            input.change(function() {
-                subjects[subject] = $(this).is(':checked');
-                localStorage.setItem(localStorageKeyName, JSON.stringify(subjects));
-                showHideAll(subjects, classes, params);
-            });
-            input.appendTo(newBlock);
-            $('<label />', { 'for': 'cb'+i, text: " " + subject }).appendTo(newBlock);
-            newBlock.appendTo(subjectDiv);
+            createCheckbox(subjects, localStorageKeyName, subjectDiv, "Subject" + i, subject, subject, function() { showHideAll(subjects, classes, params); });
         });
         subjectDiv.appendTo(newDiv);
         $(".tabMenuContentContainer").prepend(newDiv);
